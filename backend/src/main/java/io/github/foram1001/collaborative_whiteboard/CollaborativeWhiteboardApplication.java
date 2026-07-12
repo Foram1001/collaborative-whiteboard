fde4c114134c5c675df658672dd1f67d5fc82369
Foram1001 <foramkatharotiya10@gmail.com>
2026-07-12 17:26:33 +0530

    Initialize Spring Boot

---
+++ backend/src/main/java/io/github/foram1001/collaborative_whiteboard/CollaborativeWhiteboardApplication.java
@@ -0,0 +1,13 @@
+package io.github.foram1001.collaborative_whiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeWhiteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeWhiteboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/foram1001/collaborative_whiteboard/CollaborativeWhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.foram1001.collaborative_whiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeWhiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
